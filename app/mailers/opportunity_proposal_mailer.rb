401f77daa5b961da707235ef90c9192ac0a8d9d2
Nicolas Goarant <>
2025-11-21 18:35:39 +0100

    bug

--- app/mailers/opportunity_proposal_mailer.rb
+++ app/mailers/opportunity_proposal_mailer.rb
@@ -1,4 +1,3 @@
-# app/mailers/opportunity_proposal_mailer.rb
 class OpportunityProposalMailer < ApplicationMailer
   default to:   "nicolas.goarant@hotmail.fr",
           from: ENV.fetch("DEFAULT_FROM_EMAIL", "nicolas.goarant@hotmail.fr")
@@ -6,9 +5,14 @@ class OpportunityProposalMailer < ApplicationMailer
   def proposal_email
     @opportunity = params[:opportunity]
 
-    # === Ajout des pièces jointes ===
+    Rails.logger.info "[MAILER] Opportunity #{@opportunity.id} " \
+                      "photos.attached?=#{@opportunity.photos.attached?} " \
+                      "count=#{@opportunity.photos.count}"
+
     if @opportunity.photos.attached?
       @opportunity.photos.each_with_index do |photo, index|
+        Rails.logger.info "[MAILER] attaching photo #{index+1} : #{photo.filename} (#{photo.byte_size} bytes)"
+
         attachments["photo_#{index + 1}_#{photo.filename}"] = {
           mime_type: photo.blob.content_type,
           content:   photo.download
