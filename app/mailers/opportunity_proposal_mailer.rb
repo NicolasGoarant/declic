b3aced85a96e06e10f41255fa40e7b5f3c9ff1c1
Nicolas Goarant <>
2025-11-21 17:23:48 +0100

    new

--- app/mailers/opportunity_proposal_mailer.rb
+++ app/mailers/opportunity_proposal_mailer.rb
@@ -1,12 +1,16 @@
+# app/mailers/opportunity_proposal_mailer.rb
 class OpportunityProposalMailer < ApplicationMailer
+  # où tu veux recevoir les mails
   default to: "nicolas.goarant@hotmail.fr"
 
+  # Mail envoyé quand quelqu’un valide sa fiche "Proposer une opportunité"
   def proposal_email
     @opportunity = params[:opportunity]
 
-    # Pièces jointes
+    # Joindre les photos de l’opportunité (s’il y en a)
     if @opportunity.photos.attached?
       @opportunity.photos.each_with_index do |photo, index|
+        # photo.download lit le fichier depuis Active Storage
         attachments["photo_#{index + 1}#{File.extname(photo.filename.to_s)}"] = photo.download
       end
     end
