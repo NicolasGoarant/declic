01f20e34898779457121422444f7ea503ff3a564
Nicolas Goarant <>
2025-11-23 11:21:36 +0100

    Add debug logs to OpportunityProposalMailer

--- app/mailers/opportunity_proposal_mailer.rb
+++ app/mailers/opportunity_proposal_mailer.rb
@@ -5,15 +5,32 @@ class OpportunityProposalMailer < ApplicationMailer
   def proposal_email
     @opportunity = params[:opportunity]
 
-    # On ajoute les pièces jointes AVANT l'appel à `mail`
+    Rails.logger.info("MAILER DEBUG: entering proposal_email")
+    Rails.logger.info("MAILER DEBUG: opportunity id=#{@opportunity&.id.inspect} class=#{@opportunity.class.name}")
+
+    if @opportunity.respond_to?(:photos)
+      Rails.logger.info("MAILER DEBUG: @opportunity.photos.attached?=#{@opportunity.photos.attached?.inspect}")
+      Rails.logger.info("MAILER DEBUG: @opportunity.photos.size=#{@opportunity.photos.size}")
+    else
+      Rails.logger.info("MAILER DEBUG: @opportunity does not respond_to? :photos")
+    end
+
     if @opportunity.photos.attached?
       @opportunity.photos.each_with_index do |photo, index|
+        Rails.logger.info("MAILER DEBUG: attaching photo ##{index + 1} - " \
+                          "filename=#{photo.filename}, content_type=#{photo.blob.content_type}, " \
+                          "byte_size=#{photo.blob.byte_size}")
+
         attachments["opportunity_#{@opportunity.id}_photo_#{index + 1}_#{photo.filename}"] = {
           mime_type: photo.blob.content_type,
           content: photo.blob.download
         }
       end
+    else
+      Rails.logger.info("MAILER DEBUG: no photos attached to opportunity")
     end
+
+    Rails.logger.info("MAILER DEBUG: attachments keys before mail: #{attachments.keys}")
 
     mail(
       subject: "Nouvelle opportunité proposée – #{@opportunity.title.presence || 'Sans titre'}"
@@ -21,4 +38,6 @@ class OpportunityProposalMailer < ApplicationMailer
       format.html
     end
   end
+end
+d
 end
